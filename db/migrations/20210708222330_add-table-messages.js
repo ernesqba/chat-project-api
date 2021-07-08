146838d921ce2fcb5d6f0a4a25b4b6f66a01927b
dgd <guatydavid@gmail.com>
2021-07-08 19:09:13 -0400

    Creating migration for table messages

---
+++ db/migrations/20210708222330_add-table-messages.js
@@ -0,0 +1,12 @@
+const TABLE_NAME = 'messages';
+
+exports.up = (knex) => knex.schema.createTable(TABLE_NAME, (table) => {
+  table.increments().primary();
+  table.string('message').notNullable();
+  table.timestamp('created_at').notNullable().defaultTo(knex.fn.now()).notNullable();
+  table.timestamp('updated_at');
+  table.integer('users message').notNullable();
+  table.foreig('users message').references('id').inTable('users');
+});
+
+exports.down = (knex) => knex.schema.dropTable(TABLE_NAME);
