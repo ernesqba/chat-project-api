b1201658da6661b4ed6cad88671e5b5b99857871
dgd <guatydavid@gmail.com>
2021-07-08 19:44:17 -0400

    Making headers of table without spaces

--- db/migrations/20210708222330_add-table-messages.js
+++ db/migrations/20210708222330_add-table-messages.js
@@ -5,8 +5,8 @@ exports.up = (knex) => knex.schema.createTable(TABLE_NAME, (table) => {
   table.string('message').notNullable();
   table.timestamp('created_at').notNullable().defaultTo(knex.fn.now()).notNullable();
   table.timestamp('updated_at');
-  table.integer('users message').notNullable();
-  table.foreign('users message').references('id').inTable('users');
+  table.integer('users_message').notNullable();
+  table.foreign('users_message').references('id').inTable('users');
 });
 
 exports.down = (knex) => knex.schema.dropTable(TABLE_NAME);
