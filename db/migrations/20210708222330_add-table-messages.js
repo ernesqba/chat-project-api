54b01e411405c73e3154e4a48ebf6362cfe7ec26
dgd <guatydavid@gmail.com>
2021-07-08 19:11:56 -0400

    fixing typo

--- db/migrations/20210708222330_add-table-messages.js
+++ db/migrations/20210708222330_add-table-messages.js
@@ -6,7 +6,7 @@ exports.up = (knex) => knex.schema.createTable(TABLE_NAME, (table) => {
   table.timestamp('created_at').notNullable().defaultTo(knex.fn.now()).notNullable();
   table.timestamp('updated_at');
   table.integer('users message').notNullable();
-  table.foreig('users message').references('id').inTable('users');
+  table.foreign('users message').references('id').inTable('users');
 });
 
 exports.down = (knex) => knex.schema.dropTable(TABLE_NAME);
